7ae141cc109faa76fb4bb036169f2de03008520a
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-03-17 14:20:57 -0400

    Start of user input. (-not working-).

---
+++ src/test/resources/static/static/main.js
@@ -0,0 +1,55 @@
+var welcomeMsg = 'Movie App';
+document.querySelector('h1').innerText = welcomeMsg;
+
+fetch('/movies').then(resp => resp.json()).then(movies => {
+        document.querySelector('#movies').innerHTML = listMovies(movies);
+    }
+);
+
+function listMovies(json) {
+    return `${json.map(listMovie).join('\n')}`;
+};
+
+let listMovie = function(movie) {
+    return '<p>' + movie.movieID + ": " + movie.title + '</p>';
+};
+
+function postMovie() {
+    let movie = {
+        "movieID": document.getElementById("movieID").value,
+        "imdb_id": document.getElementById("imdb_id").value,
+        "title": document.getElementById("title").value,
+        "overview": document.getElementById("overview").value,
+        "releaseDate": document.getElementById("releaseDate").value,
+        "cost": document.getElementById("cost").value
+    }
+
+    console.log(movie);
+    console.log(movie.movieID);
+    console.log(movie.imdb_id);
+    console.log(movie.title);
+    console.log(movie.overview);
+    console.log(movie.releaseDate);
+    console.log(movie.cost);
+    fetch('/movies', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+    }).then((result) => {
+        if (result.status != 200) {
+            throw new Error("Bad Server Response");
+        }
+        console.log("text sent");
+        fetch('/movies').then(resp => resp.json()).then(movies => {
+            document.querySelector('#movies').innerHTML = listMovies(movies);
+        })}
+    ).catch((error) => { console.log(error); })
+}
+
+let button = document.querySelector('button');
+button.addEventListener('mouseenter', function() {
+    button.textContent = "Go!";
+})
